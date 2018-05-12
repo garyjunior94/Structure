33c2588dec9ae520ba778a655e98184d9a9b2fb4
Gary <gary.root.a@gmail.com>
2018-05-12 10:05:02 -0700

    Dynamic saving into database when press save

--- Samples/Scanner/ViewNewPatient.h
+++ Samples/Scanner/ViewNewPatient.h
@@ -12,13 +12,16 @@
 #import <Structure/Structure.h>
 
 
-@interface ViewNewPatient : UIViewController <STBackgroundTaskDelegate, UIPopoverControllerDelegate, UIGestureRecognizerDelegate, UITextFieldDelegate>
+@interface ViewNewPatient : UIViewController <STBackgroundTaskDelegate, UIPopoverControllerDelegate, UIGestureRecognizerDelegate, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) IBOutlet UILabel *dateLabel;
 
 @property (weak, nonatomic) IBOutlet UITextField *txtLastname;
 @property (weak, nonatomic) IBOutlet UITextField *txtFirstname;
 @property (weak, nonatomic) IBOutlet UITextField *txtAge;
+
+@property (weak, nonatomic) IBOutlet UITableView *tblPeople;
+@property (weak, nonatomic) IBOutlet UITableViewCell *myCell;
 
 - (IBAction)saveInfo:(id)sender;
 
--- Samples/Scanner/ViewNewPatient.mm
+++ Samples/Scanner/ViewNewPatient.mm
@@ -12,11 +12,71 @@
 @interface ViewNewPatient()
 
 @property (nonatomic, strong) DBManager *dbManager;
+@property (nonatomic, strong) NSArray *patientInfo;
+
+-(void)loadData;
 
 @end
 
 @implementation ViewNewPatient
 
+//Private
+-(void)loadData{
+    // Form the query.
+    NSString *query = @"select * from peopleInfo";
+    
+    // Get the results.
+    if (self.patientInfo != nil) {
+        self.patientInfo = nil;
+    }
+    self.patientInfo = [[NSArray alloc] initWithArray:[self.dbManager loadDataFromDB:query]];
+    
+    // Reload the table view.
+    [self.tblPeople reloadData];
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.patientInfo.count;
+}
+
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 60.0;
+}
+
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *MyIdentifier = @"MyIdentifier";
+    
+    // Dequeue the cell.
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:MyIdentifier];
+    }
+    
+    NSInteger indexOfFirstname = [self.dbManager.arrColumnNames indexOfObject:@"firstname"];
+    NSInteger indexOfLastname = [self.dbManager.arrColumnNames indexOfObject:@"lastname"];
+    NSInteger indexOfAge = [self.dbManager.arrColumnNames indexOfObject:@"age"];
+    
+    // Set the loaded data to the appropriate cell labels.
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ %@", [[self.patientInfo objectAtIndex:indexPath.row] objectAtIndex:indexOfFirstname], [[self.patientInfo objectAtIndex:indexPath.row] objectAtIndex:indexOfLastname]];
+    
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Age: %@", [[self.patientInfo objectAtIndex:indexPath.row] objectAtIndex:indexOfAge]];
+    
+    return cell;
+}
+
+// Public
 
 - (void)dealloc
 {
@@ -47,12 +107,17 @@
     self.txtFirstname.delegate = self;
     self.txtAge.delegate = self;
     
+    self.tblPeople.delegate = self;
+    self.tblPeople.dataSource = self;
+    
     NSDateFormatter *dateFormatter=[[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"yyyy-MM-dd"];
     // or @"yyyy-MM-dd hh:mm:ss a" if you prefer the time with AM/PM
     //NSLog(@"%@",[dateFormatter stringFromDate:[NSDate date]]);
     
     _dateLabel.text = [dateFormatter stringFromDate:[NSDate date]];
+    
+    [self loadData];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -87,5 +152,7 @@
     else{
         NSLog(@"Could not execute the query.");
     }
+    
+    [self loadData];
 }
 @end
